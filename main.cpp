6a4b6ef990b85719ebfa00302c0bd24af7ff995e
Andreas Zwinkau <zwinkau@mailbox.org>
2017-12-11 20:26:22 +0100

    Code cleanup

--- main.cpp
+++ main.cpp
@@ -3,29 +3,25 @@
 
 #include <main.h>
 
+// external D functions
 void d_init();
 void d_term();
 LineObserver<int>* from_d();
 
 int main () {
-  d_init();
+  d_init(); // init D runtime
 
-  vector<LineObserver<int>*> obs;
-  obs.push_back(new PostfixPrinter<int>());
-
-  auto dobj = from_d();
-  obs.push_back(dobj);
+  vector<shared_ptr<LineObserver<int> > > obs;
+  obs.push_back(make_shared<PostfixPrinter<int> >());
+  obs.push_back(shared_ptr<LineObserver<int> >(from_d()));
 
   int i = 0;
   for (string line; getline(cin, line); i++) {
-    auto begin = obs.begin();
-    auto end = obs.end();
-    for (; begin != end; ++begin) {
-      auto ob = *begin;
+    for (auto &ob : obs) {
       ob->observeLine(line.c_str(), i);
     }
   }
 
-  d_term();
+  d_term(); // shutdown D runtime (necessary?)
   return 0;
 }
